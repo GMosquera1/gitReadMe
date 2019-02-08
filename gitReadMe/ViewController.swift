5818ca97ad32994c6cd6e9d73799f82d008bc980
GMosquera1 <genesismosquera@pursuit.org>
2019-02-08 10:54:52 -0500

    first commit

---
+++ gitReadMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitReadMe
+//
+//  Created by Genesis Mosquera on 2/8/19.
+//  Copyright © 2019 Genesis Mosquera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("working on git")
+    }
+
+
+}
+
